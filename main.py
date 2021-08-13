c3a774846abbda83eed32ab2d245cba550a95637
karolisliepa <Karolis.Liepa@gmail.com>
2021-08-13 11:53:54 -0400

    Adding additional

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 import smtplib
 from datetime import date
-import config
+
 
 apiKey = "90lPqyA1NbfpaGO8OlOYxx3awDEYxiP1"
-ticker = "TSLA"
-limit = "2"
+ticker = input("Ivesk norima akcija:").upper()
+limit = "1"
 
 today = date.today()
 year = today.strftime("%Y")
@@ -13,7 +13,7 @@ month = today.strftime("%m")
 day = today.strftime("%d")
 
 my_email = "karolistest0@gmail.com"
-password = input("What's your password?")
+password = input("Password: ")
 subject = "Stocks news"
 
 api_url = f"https://api.polygon.io/v2/reference/news?limit={limit}&order=descending&sort=published_utc&ticker={ticker}&published_utc.gte={year}-{month}-{day}&apiKey={apiKey}"
@@ -41,13 +41,19 @@ for i in range(int(limit)):
     news_title = data['results'][i]['title']
     news_article_url = data['results'][i]['article_url']
     news_description = data['results'][i]['description']
+
     list1.append(news_title)
     list2.append(news_article_url)
     list3.append(news_description)
 
+    str1 = ''.join(news_title)
+    str2 = ''.join(news_article_url)
+    str3 = ''.join(news_description)
+
 msg = f"Subject:{subject}\n\n" \
-      f"News: {list1}\n" \
-      f"URL: {list2}\n" \
-    #      f"Description: {list3}" Description is just too long should check on this
+      f"News: {str1}\n" \
+      f"URL: {str2}\n" \
+      f"Description: {str3}"
 
 send_email(my_email, password, msg)
+
